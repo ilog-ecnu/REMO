c26930b69d48d22d9124e881db09cdf8fd521bdb
Intelligent Learning and Optimizaiton Group <ilog_ecnu@126.com>
2021-12-26 23:12:45 +0800

    update code

--- REMO on PlatEMO/run_REMO.m
+++ REMO on PlatEMO/run_REMO.m
@@ -1,1 +1,1 @@
-main('-algorithm',@REMO,'-problem',@DTLZ5,'-N',50,'-D',30,'-evaluation',300,'-M',3,'-run',1);
+main('-algorithm',@REMO,'-problem',@DTLZ1,'-N',50,'-D',10,'-evaluation',300,'-M',3,'-run',1);
